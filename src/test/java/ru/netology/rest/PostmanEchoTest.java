7929ef17d17d902942fae83b0f7b70a83aa5715f
Lesya <lesik_il_89@mail.ru>
2023-06-04 14:38:11 +0300

    Initinal commit

---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,24 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    void testingTheRequest() {
+        // Предусловие
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"));
+    }
+}
